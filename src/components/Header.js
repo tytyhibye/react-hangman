732b7f1d968e888dbfb06df641ea1b6068fb8f67
Tyler Bates <Fondlytyler@gmail.com>
2020-07-09 18:10:55 -0700

    update styling for more simple, cleaner design.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Header(){
   return (
-    <React.Fragment>
-      <h2> Hangman </h2>
-      <p>Click on a letter of the alphabet to guess that letter, but beware, you only have six guesses to get it right!</p>
-    </React.Fragment>
+    <img src="https://i.ibb.co/4KhK4jR/hangman.png" alt='Title'/>
+    // <React.Fragment>
+    //   <h2> Hangman </h2>
+    //   <p>Click on a letter of the alphabet to guess that letter, but beware, you only have six guesses to get it right!</p>
+    // </React.Fragment>
   );
 }
 
